0a7d38ca3a65133af46a438d00ff25948a78019a
Laurent Aimar <fenrir@videolan.org>
2004-08-22 15:01:46 +0000

    * all: include stdarg.h (needed for x264_log)
    git-svn-id: svn://svn.videolan.org/x264/trunk@38 df754926-b1dd-0310-bc7b-ec298dee348c

--- core/ppc/mc.c
+++ core/ppc/mc.c
@@ -25,6 +25,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdarg.h>
 
 #ifdef SYS_LINUX
 #include <altivec.h>
--- core/ppc/pixel.c
+++ core/ppc/pixel.c
@@ -25,6 +25,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdarg.h>
 
 #ifdef SYS_LINUX
 #include <altivec.h>
