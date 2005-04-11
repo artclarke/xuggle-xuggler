e1b747ff05b28ee786425d48be53376c620a1cdc
Eric Petit <titer@videolan.org>
2005-04-11 21:21:05 +0000

    Cygwin fixes
    git-svn-id: svn://svn.videolan.org/x264/trunk@195 df754926-b1dd-0310-bc7b-ec298dee348c

--- version.sh
+++ version.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 VER=`svnversion .`
-if [[ $VER && $VER != exported ]]
-then echo "#define X264_VERSION \" svn-$VER\"" > config.h
-else echo "#define X264_VERSION \"\"" > config.h
+if [ "x$VER" != x -a "$VER" != exported ]
+then
+  echo "#define X264_VERSION \" svn-$VER\"" > config.h
+else
+  echo "#define X264_VERSION \"\"" > config.h
 fi
