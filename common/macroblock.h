de1af4c2e5528fae5918ddf00d6ae09a68ea2222
Loren Merritt <pengvado@videolan.org>
2006-05-10 16:47:05 +0000

    msvc doesn't like C99 named array initializers
    git-svn-id: svn://svn.videolan.org/x264/trunk@522 df754926-b1dd-0310-bc7b-ec298dee348c

--- common/macroblock.h
+++ common/macroblock.h
@@ -36,20 +36,20 @@ enum macroblock_position_e
     ALL_NEIGHBORS = 0xf,
 };
 
-static const int x264_pred_i4x4_neighbors[13] =
+static const int x264_pred_i4x4_neighbors[12] =
 {
-    [I_PRED_4x4_HU]      = MB_LEFT,
-    [I_PRED_4x4_H]       = MB_LEFT,
-    [I_PRED_4x4_HD]      = MB_LEFT | MB_TOPLEFT | MB_TOP,
-    [I_PRED_4x4_DDR]     = MB_LEFT | MB_TOPLEFT | MB_TOP,
-    [I_PRED_4x4_VR]      = MB_LEFT | MB_TOPLEFT | MB_TOP,
-    [I_PRED_4x4_V]       = MB_TOP,
-    [I_PRED_4x4_VL]      = MB_TOP  | MB_TOPRIGHT,
-    [I_PRED_4x4_DDL]     = MB_TOP  | MB_TOPRIGHT,
-    [I_PRED_4x4_DC]      = MB_LEFT | MB_TOP,
-    [I_PRED_4x4_DC_LEFT] = MB_LEFT,
-    [I_PRED_4x4_DC_TOP]  = MB_TOP,
-    [I_PRED_4x4_DC_128]  = 0
+    MB_TOP,                         // I_PRED_4x4_V
+    MB_LEFT,                        // I_PRED_4x4_H
+    MB_LEFT | MB_TOP,               // I_PRED_4x4_DC
+    MB_TOP  | MB_TOPRIGHT,          // I_PRED_4x4_DDL
+    MB_LEFT | MB_TOPLEFT | MB_TOP,  // I_PRED_4x4_DDR
+    MB_LEFT | MB_TOPLEFT | MB_TOP,  // I_PRED_4x4_VR
+    MB_LEFT | MB_TOPLEFT | MB_TOP,  // I_PRED_4x4_HD
+    MB_TOP  | MB_TOPRIGHT,          // I_PRED_4x4_VL
+    MB_LEFT,                        // I_PRED_4x4_HU
+    MB_LEFT,                        // I_PRED_4x4_DC_LEFT
+    MB_TOP,                         // I_PRED_4x4_DC_TOP
+    0                               // I_PRED_4x4_DC_128
 };
 
 
