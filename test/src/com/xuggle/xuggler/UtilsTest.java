d5bb26f9f0ba98384f5389f0358b391b5270aabb
Robert Harris <trebor@xuggle.com>
2009-01-15 01:47:37 +0000

    added tests for the Utils picture to image converters

--- test/src/com/xuggle/xuggler/UtilsTest.java
+++ test/src/com/xuggle/xuggler/UtilsTest.java
@@ -27,6 +27,12 @@ import com.xuggle.xuggler.IPixelFormat;
 import com.xuggle.xuggler.ITimeValue;
 import com.xuggle.xuggler.Utils;
 
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+import java.awt.Color;
+import java.util.Random;
+
 import org.junit.*;
 import org.junit.runner.RunWith;
 
@@ -129,11 +135,162 @@ public class UtilsTest
   {
     Utils.timeValueToSamples(ITimeValue.make(1, ITimeValue.Unit.SECONDS), 0);
   }
-
+  
   @Test(expected=IllegalArgumentException.class)
   public void testTimeValueToSamplesNoTimeValue()
   {
     Utils.timeValueToSamples(null, 22050);
   }
+  
+  @Test(expected=IllegalArgumentException.class)
+  public void testVideoPictureToImageNullInput()
+  {
+    Utils.videoPictureToImage(null);
+  }
 
+  @Test(expected=IllegalArgumentException.class)
+  public void testVideoPictureToImageWrongFormatInput()
+  {
+    IVideoPicture picture = IVideoPicture.make(
+      IPixelFormat.Type.YUV420P, 50, 50);
+    Utils.videoPictureToImage(picture);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void testVideoPictureToImageIncompletePicture()
+  {
+    IVideoPicture picture = IVideoPicture.make(
+      IPixelFormat.Type.RGB32, 50, 50);
+    Utils.videoPictureToImage(picture);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void testImageToVideoPictureNullInput()
+  {
+    Utils.imageToVideoPicture(null, 0);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void testImageToVideoPictureWrongFormatInput()
+  {
+    BufferedImage image = new BufferedImage(
+      50, 50, BufferedImage.TYPE_INT_RGB);
+    Utils.imageToVideoPicture(image, 0);
+  }
+
+  @Test
+  public void testImageToImageSolidColor()
+  {
+    int w = 50;
+    int h = 50;
+    int gray  = Color.GRAY .getRGB();
+
+    // construct an all gray image
+
+    BufferedImage image1 = new BufferedImage(
+      w, h, BufferedImage.TYPE_INT_ARGB);
+    for (int x = 0; x < w; ++x)
+      for (int y = 0; y < h; ++y)
+        image1.setRGB(x, y, gray);
+
+    // convert image1 to a picture and then back to image2
+
+    BufferedImage image2 = Utils.videoPictureToImage(
+      Utils.imageToVideoPicture(image1, 0));
+
+    // test that all the pixels in image2 are gray, but not black or
+    // white
+
+    for (int x = 0; x < w; ++x)
+      for (int y = 0; y < h; ++y)
+      {
+        int pixel = image2.getRGB(x, y);
+        assertTrue("color value missmatch", pixel == gray);
+      }
+  }
+
+  @Test
+  public void testImageToImageRandomColor()
+  {
+    int w = 50;
+    int h = 50;
+    Random rnd = new Random();
+
+    // construct an image of random colors
+
+    BufferedImage image1 = new BufferedImage(
+      w, h, BufferedImage.TYPE_INT_ARGB);
+    for (int x = 0; x < w; ++x)
+      for (int y = 0; y < h; ++y)
+      {
+        Color c = new Color(rnd.nextInt(255), 
+          rnd.nextInt(255), rnd.nextInt(255));
+        image1.setRGB(x, y, c.getRGB());
+      }
+
+    // convert image1 to a picture and then back to image2
+
+    BufferedImage image2 = Utils.videoPictureToImage(
+      Utils.imageToVideoPicture(image1, 0));
+
+    // test that all the pixels in image2 are the same as image1
+
+    for (int x = 0; x < w; ++x)
+      for (int y = 0; y < h; ++y)
+      {
+        int pixel1 = image1.getRGB(x, y);
+        int pixel2 = image2.getRGB(x, y);
+        assertTrue("color value missmatch", pixel1 == pixel2);
+      }
+  }
+
+  @Test
+  public void testPictureToPictureWithRotate()
+  {
+    // note that the image is square in this test to make rotation
+    // easier to handle
+
+    int size = 50;
+    int black = Color.BLACK.getRGB();
+    int white = Color.WHITE.getRGB();
+
+    // construct an image with black and white stripped columns
+
+    BufferedImage image1 = new BufferedImage(
+      size, size, BufferedImage.TYPE_INT_ARGB);
+    for (int x = 0; x < size; ++x)
+      for (int y = 0; y < size; ++y)
+      {
+        int color = x % 2 == 0 ? black : white;
+        image1.setRGB(x, y, color);
+      }
+
+    // convert image1 to a picture and then back to image2
+
+    BufferedImage image2 = Utils.videoPictureToImage(
+      Utils.imageToVideoPicture(image1, 0));
+
+    // rotae image2
+
+    AffineTransform t = AffineTransform.getRotateInstance(
+      Math.PI/2, image2.getWidth() / 2, image2.getHeight() / 2);
+    AffineTransformOp ato = new AffineTransformOp(t, 
+      AffineTransformOp.TYPE_BICUBIC);
+    image2 = ato.filter(image2, null);
+
+    // convert image2 to a picture and then back to image3
+
+    BufferedImage image3 = Utils.videoPictureToImage(
+      Utils.imageToVideoPicture(image2, 0));
+
+    // test that image3 now contains stripped rows (not columns)
+
+    for (int x = 0; x < size; ++x)
+      for (int y = 0; y < size; ++y)
+      {
+        int pixel = image3.getRGB(x, y);
+        int color = y % 2 == 0 ? black : white;
+        assertTrue("color value missmatch", pixel == color);
+      }
+  }
 }
