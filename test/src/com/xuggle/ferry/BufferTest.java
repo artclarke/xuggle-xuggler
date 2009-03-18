7d2ea9d15f20b7bde7f77e2e71ba474a67bb68b6
Art Clarke <aclarke@xuggle.com>
2009-03-18 03:26:06 +0000

    add a test to make sure that IntBuffers maintain references in Java to ByteBuffers when a view is made

--- test/src/com/xuggle/ferry/BufferTest.java
+++ test/src/com/xuggle/ferry/BufferTest.java
@@ -303,6 +303,10 @@ public class BufferTest
     jbuf.put(0, (byte)0xFF);
   }
 
+  /**
+   * This test makes sure that Xuggler sets the byte order of the
+   * returned ByteBuffer to native order
+   */
   @Test
   public void testByteOrderIsCorrect()
   {
@@ -321,7 +325,6 @@ public class BufferTest
     jbuf.put(2, (byte)0xFF);
     jbuf.put(3, (byte)0);
     
-    log.debug("Native order: {}", ByteOrder.nativeOrder());
     int bigOrderVal = 0xFF00FF00;
     int littleOrderVal = 0x00FF00FF;
     int expectedVal;
@@ -350,6 +353,77 @@ public class BufferTest
     val = ibuf.get(0);
     assertEquals(littleOrderVal, val);
     
+  }
 
+  /**
+   * This is a paranoia test really testing java.nio.ByteBuffers.
+   * 
+   * If you make a view of a ByteBuffer (ByteBuffer.asIntBuffer for example),
+   * we want to make sure the resulting IntBuffer maintains a reference
+   * back to the original ByteBuffer.
+   * 
+   */
+  @Test(timeout=2*60*1000)
+  public void testIntBuffersMaintainReferenceToByteBuffer()
+  {
+    // make sure we don't have any pinned objects lying around
+    while(JNIWeakReference.getMgr().getNumPinnedObjects()>0)
+    {
+      System.gc();
+      JNIWeakReference.getMgr().gc();
+    }
+    assertEquals(0, JNIWeakReference.getMgr().getNumPinnedObjects());
+
+    // now start the test
+    IBuffer buf = IBuffer.make(null, 1024*1024);
+    assertNotNull(buf);
+
+    // This will create a reference
+    java.nio.ByteBuffer jbuf = buf.getByteBuffer(0, buf.getBufferSize());
+
+    // kill the xuggler refcount in the IBuffer
+    buf.delete();
+
+    // this is the test; we want to know if creating an IntBuffer
+    // ends up holding a reference to the underlying jbuf
+    java.nio.IntBuffer ibuf = jbuf.asIntBuffer();
+    ibuf.put(0, 15);
+
+    // release the reference to the bytebuffer
+    jbuf = null;
+
+    assertEquals(15, ibuf.get(0));
+    
+    // now we're going to try for force a gc
+    byte[] arr;
+    for(int i = 0; i < 1000; i++)
+    {
+      arr = new byte[1024*1024]; 
+      System.gc();
+      JNIWeakReference.getMgr().gc();
+      arr[0] = (byte)1;
+      ibuf.put(i, 16);
+    }
+    
+    // even though the buf and jbuf are now dead, the ibuf should
+    // be forcing one buffer to be pinned.
+    assertEquals(1, JNIWeakReference.getMgr().getNumPinnedObjects());
+    
+    // this is here to make sure the compiler doesn't optimize
+    // away ibuf
+    assertEquals(16, ibuf.get(999));
+    // release the ibuf
+    ibuf= null;
+    // try to force another gc to make sure ibuf is now collected
+    while(JNIWeakReference.getMgr().getNumPinnedObjects()>0)
+    {
+      arr = new byte[1024*1024]; 
+      System.gc();
+      JNIWeakReference.getMgr().gc();
+      arr[0] = (byte)1;
+    }
+
+    // and make sure the ibuf is collected
+    assertEquals(0, JNIWeakReference.getMgr().getNumPinnedObjects());
   }
 }
