21c9c6525bff06e0be2a8bc698d0a1b788d29310
Robert Harris <trebor@xuggle.com>
2009-04-17 21:09:04 +0000

    removed extra carrage returns

--- src/com/xuggle/xuggler/ISimpleMediaFile.java
+++ src/com/xuggle/xuggler/ISimpleMediaFile.java
@@ -1,23 +1,27 @@
 /*
  * Copyright (c) 2008-2009 by Xuggle Inc. All rights reserved.
  *
- * It is REQUESTED BUT NOT REQUIRED if you use this library, that you let 
- * us know by sending e-mail to info@xuggle.com telling us briefly how you're
- * using the library and what you like or don't like about it.
+ * It is REQUESTED BUT NOT REQUIRED if you use this library, that you
+ * let us know by sending e-mail to info@xuggle.com telling us briefly
+ * how you're using the library and what you like or don't like about
+ * it.
  *
- * This library is free software; you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License as published by the Free Software
- * Foundation; either version 2.1 of the License, or (at your option) any later
- * version.
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License along
- * with this library; if not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
  */
+
 package com.xuggle.xuggler;
 
 import com.xuggle.xuggler.IAudioSamples;
@@ -38,8 +42,8 @@ import com.xuggle.xuggler.ITimeValue;
  * object.
  * 
  * @author aclarke
- *
  */
+
 public interface ISimpleMediaFile
 {
 
@@ -406,5 +410,4 @@ public interface ISimpleMediaFile
    * @return The URL, or null if unknown.
    */
   String getURL();
-  
 }
