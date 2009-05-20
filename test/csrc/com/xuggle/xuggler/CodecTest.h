ead4c0b5b5fa57fd4439a0c3c2658bf57953289a
Robert Harris <trebor@xuggle.com>
2009-05-20 17:45:07 +0000

    change of LGPL to AGPL

--- test/csrc/com/xuggle/xuggler/CodecTest.h
+++ test/csrc/com/xuggle/xuggler/CodecTest.h
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Xuggler.
+ * 
+ * Xuggler is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ * 
+ * Xuggler is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public
+ * License along with Xuggler.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __CODEC_TEST_H__
 #define __CODEC_TEST_H__
 
