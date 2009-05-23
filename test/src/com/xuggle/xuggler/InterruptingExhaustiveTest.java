14fcf0875727d909de451facfd3c0d046b0a65cb
Art Clarke <aclarke@xuggle.com>
2009-05-23 19:25:41 +0000

    - fix stupid timing issues with interrupting test (hopefully).  did I mention I hate threading, which is why other stuff is 'event' driven.

--- test/src/com/xuggle/xuggler/InterruptingExhaustiveTest.java
+++ test/src/com/xuggle/xuggler/InterruptingExhaustiveTest.java
@@ -3,6 +3,8 @@ package com.xuggle.xuggler;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.junit.Assert.*;
 
@@ -14,49 +16,59 @@ import static org.junit.Assert.*;
  */
 public class InterruptingExhaustiveTest
 {
-  
+  private final Logger log = LoggerFactory.getLogger(this.getClass());
+ 
   // needs a long time out because if running in an exhaustive
   // test on a slow machine (oh like our build server) it can
   // sometime fail.
   @Test(timeout=120*1000)
   public void testXugglerSupportInterruptions() throws InterruptedException
   {
+    final AtomicBoolean testStarted = new AtomicBoolean(false);
     final AtomicBoolean testPassed = new AtomicBoolean(false);
+    final Object lock = new Object();
     Thread thread = new Thread(
         new Runnable(){
           public void run()
           {
-            synchronized(this) {
-              this.notifyAll();
-            }
             IContainer container = IContainer.make();
 
+            synchronized(lock) {
+              testStarted.set(true);
+              lock.notifyAll();
+            }
+            log.info("About to call blocking ffmpeg method");
             int retval = container.open(
 //                "fixtures/testfile.flv",
                "udp://127.0.0.1:12345/notvalid.flv?localport=28302",
                 IContainer.Type.READ,
                 null, true, false);
+            log.info("ffmpeg method returned");
             // we should return from this method in an
             // interrupted state.
             if (Thread.interrupted()) {
+              log.info("thread was interrupted");
               // clear the interrupt
-              synchronized(this) {
-                this.notifyAll();
-              }
               if (retval < 0)
                 testPassed.set(true);
+              else
+                log.info("but test did not pass");
             }
           }},
         "xuggler thread");
     
-    synchronized(thread) {
+    synchronized(lock) {
       thread.start();
-      thread.wait();
+      while(!testStarted.get()) {
+        log.info("waiting for start");
+        lock.wait();
+      }
     }
-    synchronized(thread) {
-      thread.interrupt();
-      thread.wait();
-    }
+    // give the thread about a half a second to get into ffmpeg land
+    Thread.sleep(500);
+    log.info("about to interrupt ffmpeg-calling thread");
+    thread.interrupt();
+    log.info("interrupted ffmpeg-calling thread");
     thread.join();
     assertTrue("test did not pass", testPassed.get());
   }
