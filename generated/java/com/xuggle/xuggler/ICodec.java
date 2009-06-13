f5dc6651487c4bd40d9294b47a02ce1f8b00d21d
Art Clarke <aclarke@xuggle.com>
2009-06-13 16:12:06 +0000

    add EA decoder

--- generated/java/com/xuggle/xuggler/ICodec.java
+++ generated/java/com/xuggle/xuggler/ICodec.java
@@ -753,6 +753,7 @@ public class ICodec extends RefCounted {
     CODEC_ID_TMV,
     CODEC_ID_V210,
     CODEC_ID_DPX,
+    CODEC_ID_MAD,
     CODEC_ID_PCM_S16LE(XugglerJNI.ICodec_CODEC_ID_PCM_S16LE_get()),
     CODEC_ID_PCM_S16BE,
     CODEC_ID_PCM_U16LE,
