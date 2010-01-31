57fea6b2ea5ecab9a924b9bbe1687e36b379f406
Art Clarke <aclarke@xuggle.com>
2010-01-31 18:51:32 +0000

    - And tested H264 publishing and it works.  Woo hoo!  Again just with Wowza.

--- test/src/com/xuggle/xuggler/RTMPPublishingExhaustiveTest.java
+++ test/src/com/xuggle/xuggler/RTMPPublishingExhaustiveTest.java
@@ -30,7 +30,7 @@ public class RTMPPublishingExhaustiveTest
   private static final String PROTOCOL_NAME = "rtmp";
 
   @Test
-  public void testRTMPPublish() throws ParseException
+  public void testRTMPPublishSorenson() throws ParseException
   {
     if (!rtmpPortOpen())
       return;
@@ -60,6 +60,61 @@ public class RTMPPublishingExhaustiveTest
         "12000000",
         "--vquality",
         "0",
+        "--realtime", // this will stream out to an RTMP server
+        "fixtures/testfile_videoonly_20sec.flv",
+        url
+    };
+    
+    Converter converter = new Converter();
+    
+    Options options = converter.defineOptions();
+  
+    CommandLine cmdLine = converter.parseOptions(options, args);
+    assertTrue("all commandline options successful", cmdLine != null);
+    
+    final long startTime = System.nanoTime();
+    converter.run(cmdLine);
+    final long endTime = System.nanoTime();
+    final long delta = endTime - startTime;
+    // 20 second test file;
+    assertTrue("did not take long enough", delta >= 18L*1000*1000*1000);
+    System.err.println("Total time taken: " + delta);
+    assertTrue("took too long", delta <= 60L*1000*1000*1000);
+    
+  }
+  @Test
+  public void testRTMPPublishH264() throws ParseException
+  {
+    if (!rtmpPortOpen())
+      return;
+    final String url =
+      PROTOCOL_NAME + "://" + SERVER_NAME + ":" + SERVER_PORT + "/" + APP_NAME + "/" + "mp4:"+ STREAM_NAME;
+    
+    String[] args = new String[]{
+        "--containerformat",
+        "flv",
+        "--acodec",
+        "libfaac",
+        "--asamplerate",
+        "22050",
+        "--achannels",
+        "1",
+        "--abitrate",
+        "64000",
+        "--aquality",
+        "0",
+        "--vcodec",
+        "libx264",
+        "--vscalefactor",
+        "1.0",
+        "--vbitrate",
+        "300000",
+        "--vbitratetolerance",
+        "12000000",
+        "--vquality",
+        "0",
+        "--vpreset",
+        "fixtures/"+this.getClass().getName()+"_h264.preset",
         "--realtime", // this will stream out to an RTMP server
         "fixtures/testfile_videoonly_20sec.flv",
         url
