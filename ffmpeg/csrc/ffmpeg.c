d05a438015b80f450f74961df2713a6093624ef2
Stas.Oskin@gmail.com <Stas.Oskin@gmail.com@9f8918ac-6a95-11de-be4f-b1b98ff093f4>
2011-05-18 00:00:16 +0000

    READ ONLY: Daily import from FFMPEG git
    git-svn-id: https://xuggle-ffmpeg.googlecode.com/svn/trunk@948 9f8918ac-6a95-11de-be4f-b1b98ff093f4

--- ffmpeg/csrc/ffmpeg.c
+++ ffmpeg/csrc/ffmpeg.c
@@ -171,8 +171,6 @@ static int loop_output = AVFMT_NOOUTPUTLOOP;
 static int qp_hist = 0;
 #if CONFIG_AVFILTER
 static char *vfilters = NULL;
-#else
-static unsigned int sws_flags = SWS_BICUBIC;
 #endif
 
 static int intra_only = 0;
@@ -289,6 +287,7 @@ typedef struct AVOutputStream {
     int resample_pix_fmt;
 
     float frame_aspect_ratio;
+
     /* forced key frames */
     int64_t *forced_kf_pts;
     int forced_kf_count;
@@ -1642,7 +1641,7 @@ static int output_packet(AVInputStream *ist, int ist_index,
         }
 
 #if CONFIG_AVFILTER
-        if(ist->st->codec->codec_type == AVMEDIA_TYPE_VIDEO){
+        if(ist->st->codec->codec_type == AVMEDIA_TYPE_VIDEO) {
             for(i=0;i<nb_ostreams;i++) {
                 ost = ost_table[i];
                 if (ost->input_video_filter && ost->source_index == ist_index) {
@@ -1786,7 +1785,7 @@ static int output_packet(AVInputStream *ist, int ist_index,
                     cont:
                     frame_available = (ist->st->codec->codec_type == AVMEDIA_TYPE_VIDEO) &&
                                        ost->output_video_filter && avfilter_poll_frame(ost->output_video_filter->inputs[0]);
-                    if(ost->picref)
+                    if (ost->picref)
                         avfilter_unref_buffer(ost->picref);
                 }
 #endif
@@ -3457,6 +3456,7 @@ static void opt_input_file(const char *filename)
     nb_input_files++;
 
     video_channel = 0;
+    top_field_first = -1;
 
     av_freep(&video_codec_name);
     av_freep(&audio_codec_name);
@@ -3537,8 +3537,8 @@ static void new_video_stream(AVFormatContext *oc, int file_idx)
         ost->frame_aspect_ratio = frame_aspect_ratio;
         frame_aspect_ratio = 0;
 #if CONFIG_AVFILTER
-        ost->avfilter= vfilters;
-        vfilters= NULL;
+        ost->avfilter = vfilters;
+        vfilters = NULL;
 #endif
     }
 
